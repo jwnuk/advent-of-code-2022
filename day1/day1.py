3b5fb0efd7d1b8877c988f4d14f16f3d4cad8c16
Julka <julia.m.wnuk@gmail.com>
2022-12-27 20:48:11 +0100

    Added second part for day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,26 @@
 input = open("input.txt")
 
-max_e = 0
+top1 = 0
+top2 = 0
+top3 = 0
 elf = 0
 
 for i in input:
     if len(i) > 1:
         elf += int(i)
     else:
-        if elf > max_e:
-            max_e = elf
+        if elf > top1:
+            top3 = top2
+            top2 = top1
+            top1 = elf
+        elif elf > top2:
+            top3 = top2
+            top2 = elf
+        elif elf > top3:
+            top3 = elf
         elf = 0
 
-print(max_e)
+print(top1)
+print(top2)
+print(top3)
+print(top1 + top2 + top3)
