a1bd4712869f6a5125be7e1a21ca6d597b7e8144
Julka <julia.m.wnuk@gmail.com>
2022-12-29 20:18:21 +0100

    Added second part of task #2

--- day2/day2.py
+++ day2/day2.py
@@ -39,3 +39,47 @@ for i in guide:
         loses -= 6
 
 print("score = ", rocks + papers + scissors + wins + ties)
+
+
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+"""
+1, A == rock
+2, B == paper
+3, C == scissors
+
+X == lose
+Y == draw
+Z == win
+
+rock    paper   scissors
+A Y     A Z     A X
+B X     B Y     B Z
+C Z     C X     C Y
+"""
+
+guide_2 = open("input.txt")
+
+wins_2 = 0
+ties_2 = 0
+loses_2 = 0
+
+rocks_2 = 0
+papers_2 = 0
+scissors_2 = 0
+
+for j in guide_2:
+    if j[2] == "X":
+        loses_2 -= 6
+    elif j[2] == "Y":
+        ties_2 += 3
+    elif j[2] == "Z":
+        wins_2 += 6
+
+    if (j[0:3] == "A Y") or (j[0:3] == "B X") or (j[0:3] == "C Z"):
+        rocks_2 += 1
+    elif (j[0:3] == "A Z") or (j[0:3] == "B Y") or (j[0:3] == "C X"):
+        papers_2 += 2
+    elif (j[0:3] == "A X") or (j[0:3] == "B Z") or (j[0:3] == "C Y"):
+        scissors_2 += 3
+
+print("score_2 = ", rocks_2 + papers_2 + scissors_2 + wins_2 + ties_2)
