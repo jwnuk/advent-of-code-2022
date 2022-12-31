1731ca0b3a0212b60142d1e5cd401a8cd67b1dd9
Julka <julia.m.wnuk@gmail.com>
2022-12-31 17:29:06 +0100

    Added first part of task #4

---
+++ day04/day04.py
@@ -0,0 +1,26 @@
+sections = open("input.txt")
+
+pairs = []
+
+for line in sections:
+    pairs.append(line[0:len(line)-1].split(","))
+
+repeating_assignments = []
+
+for pair in pairs:
+    pair[0] = pair[0].split("-")
+    pair[1] = pair[1].split("-")
+    
+    min1 = int(pair[0][0])
+    min2 = int(pair[1][0])
+    max1 = int(pair[0][1])
+    max2 = int(pair[1][1])
+
+    if (min1 <= min2) and (max1 >= max2):
+        repeating_assignments.append(pair)
+    elif (min2 <= min1) and (max2 >= max1):
+        repeating_assignments.append(pair)
+    else:
+        continue
+
+print("Number of assignments that fully contains the other from pair: ", len(repeating_assignments))
