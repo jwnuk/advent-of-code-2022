012ebf98cd374d650202235dc6c78b7d71b19202
Julka <julia.m.wnuk@gmail.com>
2022-12-31 15:54:11 +0100

    Added second part of task #3

--- day3/day3.py
+++ day3/day3.py
@@ -19,24 +19,21 @@ letters_dict = {}
 for ix, letter in enumerate(letters_list):
     letters_dict[letter] = ix+1
 
-r1_letters = []
-r2_letters = []
+def get_distinct_letters(items_list):
+    letters_list = []
 
-for item in rucksack_1:
-    letters = set()
+    for item in items_list:
+        letters = set()
 
-    for letter in item:
-        letters.add(letter)
+        for letter in item:
+            letters.add(letter)
+
+        letters_list.append(list(letters))
     
-    r1_letters.append(list(letters))
+    return letters_list
 
-for item in rucksack_2:
-    letters = set()
-
-    for letter in item:
-        letters.add(letter)
-    
-    r2_letters.append(list(letters))
+r1_letters = get_distinct_letters(rucksack_1)
+r2_letters = get_distinct_letters(rucksack_2)
 
 priorities_sum = 0
 
@@ -51,3 +48,41 @@ for ix, item in enumerate(r1_letters):
             break
 
 print("Total priorities: ", priorities_sum)
+
+# part two
+
+elves = open("input.txt")
+elves_list = []
+
+for i in elves:
+    elves_list.append(i)
+
+elves_1, elves_2, elves_3 = [], [], []
+
+for ix, item in enumerate(elves_list):
+    if (ix + 1) % 3 == 1:
+        elves_1.append(item[0:len(item)-1])
+    elif (ix + 1) % 3 == 2:
+        elves_2.append(item[0:len(item)-1])
+    else:
+        elves_3.append(item[0:len(item)-1])
+
+e1_letters = get_distinct_letters(elves_1)
+e2_letters = get_distinct_letters(elves_2)
+e3_letters = get_distinct_letters(elves_3)
+
+priorities_sum_2 = 0
+
+for ix, item in enumerate(e1_letters):
+
+    for letter in item:
+        if e2_letters[ix].count(letter) == 0:
+            continue
+        elif e3_letters[ix].count(letter) == 0:
+            continue
+        else:
+            priority = letters_dict.get(letter)
+            priorities_sum_2 += priority
+            break
+
+print("Total priorities_2: ", priorities_sum_2)
