f1b2bfae127e9564e95c4725ecfdce4975fe7e55
Julka <julia.m.wnuk@gmail.com>
2022-12-31 17:42:23 +0100

    Added second part of task #4

--- day04/day04.py
+++ day04/day04.py
@@ -6,6 +6,7 @@ for line in sections:
     pairs.append(line[0:len(line)-1].split(","))
 
 repeating_assignments = []
+overlapping_assignments = []
 
 for pair in pairs:
     pair[0] = pair[0].split("-")
@@ -20,7 +21,11 @@ for pair in pairs:
         repeating_assignments.append(pair)
     elif (min2 <= min1) and (max2 >= max1):
         repeating_assignments.append(pair)
-    else:
-        continue
+
+    if (min1 >= min2) and (min1 <= max2):
+        overlapping_assignments.append(pair)
+    elif (min2 >= min1) and (min2 <= max1):
+        overlapping_assignments.append(pair)
 
 print("Number of assignments that fully contains the other from pair: ", len(repeating_assignments))
+print("Number of overlapping pairs of assignments: ", len(overlapping_assignments))
