7bf8bb0dbe6ac205c4a040f4af9f872e9edd4ade
Julka <julia.m.wnuk@gmail.com>
2023-01-01 22:42:18 +0100

    Added second part of task #5

--- day05/day05.py
+++ day05/day05.py
@@ -15,17 +15,20 @@ for line in crates_input:
         moves.append(line)
 
 n_stacks = crates[len(crates)-1]
-stacks_dict = {}
+stacks_dict, stacks_dict_2 = {}, {}
 
 for char in n_stacks:
     if char.isnumeric():
         stacks_dict.update({int(char): []})
+        stacks_dict_2.update({int(char): []})
 
 for n in stacks_dict.keys():
     for line in crates:
         if line[(n-1)*4] == "[":
             stacks_dict[n].append(line[(n-1)*4+1])
+            stacks_dict_2[n].append(line[(n-1)*4+1])
     stacks_dict[n].reverse()
+    stacks_dict_2[n].reverse()
 
 def move_items(move=list, stacks=dict):
     stack = int(move[3])
@@ -46,4 +49,27 @@ top_items = str("")
 for stack in stacks_dict.values():
     top_items += stack[-1]
 
-print("Items on top of the stacks: ", top_items)
+print("Items on top of the stacks (part 1): ", top_items)
+
+# part two
+
+def move_multiple_items(move=list, stacks=dict):
+    stack = int(move[3])
+    new_stack = int(move[5])
+    n_items = int(move[1])
+
+    stacks[new_stack].extend(stacks[stack][-n_items:])
+    stacks[stack] = stacks[stack][:-n_items]
+
+    return stacks
+
+for ix, line in enumerate(moves):
+    move = moves[ix].strip("\n").split(" ")
+    stacks_dict_2 = move_multiple_items(move, stacks_dict_2)
+
+top_items_2 = str("")
+
+for stack in stacks_dict_2.values():
+    top_items_2 += stack[-1]
+
+print("Items on top of the stacks (for part 2): ", top_items_2)
