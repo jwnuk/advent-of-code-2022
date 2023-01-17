2ca3f9d7ea3dcd9481b0d56699d3a254f98c8755
Julka <julia.m.wnuk@gmail.com>
2023-01-17 01:10:57 +0100

    Added second part for task #9

--- day09/day09.py
+++ day09/day09.py
@@ -61,4 +61,59 @@ for move in moves:
             
             tail_positions.append(new_T_position(T_position))
 
-print("Positions visited by tail at least once:", len(set(tail_positions)))
+print("Positions visited by tail at least once (for two points):", len(set(tail_positions)))
+
+# part 2
+
+knots_positions = [[1000, 1000] for i in range(10)]
+positions_9 = [start_point]
+
+def check_dist_and_update_position_2(position_1=list, position_2=list):
+    distances = check_T_H_dist(position_1, position_2)
+
+    if distances[0] > 1:
+        direction = "R"
+    elif distances[0] < -1:
+        direction = "L"
+    elif distances[1] > 1:
+        direction = "U"
+    else:
+        direction = "D"
+
+    if abs(distances[0]) > 1:
+        position_2 = update_position(position_2, direction)
+
+        if distances[1] > 0:
+            position_2 = update_position(position_2, "U")
+        elif distances[1] < 0:
+            position_2 = update_position(position_2, "D")
+
+    elif abs(distances[1]) > 1:
+        position_2 = update_position(position_2, direction)
+
+        if distances[0] > 0:
+            position_2 = update_position(position_2, "R")
+        elif distances[0] < 0:
+            position_2 = update_position(position_2, "L")
+
+for move in moves:
+    
+    direction = move[0]
+    steps = int(move[1])
+
+    for i in range(steps):
+        # update head position
+        pos_H = knots_positions[0]
+        knots_positions[0] = update_position(pos_H, direction)
+        
+        # check distance between H and T and update T position if necessary
+        for n in range(1, 10):
+            pos_1 = knots_positions[n-1]
+            pos_2 = knots_positions[n]
+            
+            check_dist_and_update_position_2(pos_1, pos_2)
+            
+            if n == 9:
+                positions_9.append(new_T_position(knots_positions[9]))
+
+print("Positions visited by tail at least once (for ten points):", len(set(positions_9)))
